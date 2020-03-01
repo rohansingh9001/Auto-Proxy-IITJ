f62ac48c9db6494e51bd8b4fc210b31eccaebe23
Rohan Singh <54505869+rohansingh9001@users.noreply.github.com>
2020-03-01 06:30:23 +0530

    Update INSTALL.bat

--- INSTALL.bat
+++ INSTALL.bat
@@ -1,6 +1,6 @@
 @echo off
 pip install --user --requirement requirements.txt
-copy "RUN.lnk" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp"
+copy "RUN.bat" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp"
 python register.py
 python auto_proxy.py
 
